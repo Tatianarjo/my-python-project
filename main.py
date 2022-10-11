3e9fd1ee0903b6ed53b985c567ba0a3e56f6e4ea
Tatiana Johnson <Tatianarjo@gmail.com>
2022-10-10 21:32:53 -0500

    added variables

--- main.py
+++ main.py
@@ -6,5 +6,16 @@
 
 ##String concatenation  - Adding strings together
 
-print("20 days are " + str(50) + " minutes")
+#print("20 days are " + str(50) + " minutes")
+#print(f"20 days are {50} minutes")
+print(f"20 days are {20 * 24 * 60} minutes")
+
 #print(20 * 24 * 60)
+
+#Variables
+calculation_to_seconds = 24 * 60 * 60 
+
+print(f"20 days are {20 * calculation_to_seconds} seconds")
+print(f"20 days are {50 * 24 * 60 * 60} seconds")
+print(f"20 days are {50 * 24 * 60 * 60} seconds")
+
